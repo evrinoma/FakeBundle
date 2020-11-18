df423d9e50080c81645af4d2843f6fa7a6a111ee
evrinoma <evrinoma@gmail.com>
2020-11-18 10:12:57 +0300

    fix controller options

--- src/Controller/FakeApiController.php
+++ src/Controller/FakeApiController.php
@@ -85,7 +85,7 @@ final class FakeApiController extends AbstractApiController
     }
 
     /**
-     * @Rest\Get("/api/fake/services", name="api_fake_service")
+     * @Rest\Get("/api/fake/services", name="api_fake_service", options={"expose"=true})
      * @SWG\Get(tags={"fake"})
      * @SWG\Response(response=200,description="Get fake service")
      * @SWG\Parameter(
@@ -119,7 +119,7 @@ final class FakeApiController extends AbstractApiController
     }
 
     /**
-     * @Rest\Get("/api/fake/entity_type", name="api_fake_entity_type")
+     * @Rest\Get("/api/fake/entity_type", name="api_fake_entity_type", options={"expose"=true})
      * @SWG\Get(tags={"fake"})
      * @SWG\Response(response=200,description="Get fake entity")
      *
@@ -131,7 +131,7 @@ final class FakeApiController extends AbstractApiController
     }
 
     /**
-     * @Rest\Get("/api/fake/group", name="api_fake_group")
+     * @Rest\Get("/api/fake/group", name="api_fake_group", options={"expose"=true})
      * @SWG\Get(tags={"fake"})
      * @SWG\Response(response=200,description="Get fake group")
      *
